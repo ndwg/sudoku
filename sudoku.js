1ecde1c3391c79414cab2691930e53bacff8f4bc
Nathan M Silva <nms204@scarletmail.rutgers.edu>
2022-06-03 21:06:12 -0400

    Added user input to puzzle
    User can now change puzzle tile values using keyboard (only numerical inputs are accepted). The tile being changed is highlighted.

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,8 @@
 var puzzle = [];
+var currentTile;
+var checker = false;
+
+document.addEventListener("keydown", changeValue);
 
 window.onload = function(){
     setPuzzle();
@@ -11,8 +15,27 @@ function setPuzzle(){
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             document.getElementById("puzzle").append(tile);
+            tile.addEventListener("click",clickTile);
             row.push(tile);
         }
         puzzle.push(row);
+    }
+}
+
+function chooseNumbers(){}
+
+function clickTile(){
+    if(checker){
+        currentTile.style.backgroundColor = "";
+    }
+    currentTile = this;
+    currentTile.style.backgroundColor = "yellow";
+
+    checker = true;
+}
+
+function changeValue(input){
+    if(input.key == 1 || input.key == 2 || input.key == 3 || input.key == 4 ||input.key == 5 ||input.key == 6 ||input.key == 7 ||input.key == 8 ||input.key == 9){
+        currentTile.innerText = input.key;
     }
 }
