9e8832eccec81326cae1b7e96b547554f93c75de
Nathan M Silva <nms204@scarletmail.rutgers.edu>
2022-06-07 16:45:53 -0400

    Began creating sudoku solutions
    Began work on the code that will generate a sudoku puzzle solution. Still a work in progress.

--- sudoku.js
+++ sudoku.js
@@ -30,9 +30,104 @@ function setPuzzle(){
         }
         puzzle.push(row);
     }
+
+    chooseNumbers();
 }
 
-function chooseNumbers(){}
+function chooseNumbers(){
+    configureGrid(0,3,0,3);
+    configureGrid(3,6,3,6);
+    configureGrid(6,9,6,9);
+    configureComplexGrid(0,3,6,9);
+    configureComplexGrid(6,9,0,3);
+    configureComplexGrid(0,3,3,6);
+    configureComplexGrid(3,6,0,3);
+    configureComplexGrid(3,6,6,9);
+    configureComplexGrid(6,9,3,6);
+    console.log(puzzle);
+}
+
+function configureGrid(lr,ur,lc,uc){
+    var usedNums = [];
+
+    for(let r = lr; r < ur; r++){
+        for(let c = lc; c < uc; c++){
+            let randomNum = Math.floor(Math.random()*9) + 1;
+            //puzzle[r][c].innerText = randomNum;
+            puzzle[r][c] = randomNum;
+            if(usedNums[randomNum] == 1) c--;
+            usedNums[randomNum] = 1;
+        }
+    }
+}
+
+function configureComplexGrid(lr,ur,lc,uc){
+    var potentials = [];
+
+    for(let r = lr; r < ur; r++){
+        for(let c = lc; c < uc; c++){
+            var tilePotentials = new Set;
+
+            tilePotentials.add(1);
+            tilePotentials.add(2);
+            tilePotentials.add(3);
+            tilePotentials.add(4);
+            tilePotentials.add(5);
+            tilePotentials.add(6);
+            tilePotentials.add(7);
+            tilePotentials.add(8);
+            tilePotentials.add(9);
+
+            for(let i = 0; i < 9; i++){
+                tilePotentials.delete(puzzle[r][i]);
+                tilePotentials.delete(puzzle[i][c]);
+            }
+
+            potentials.push(tilePotentials);
+        }
+    }
+
+    console.log(potentials);
+
+    for(let i = 0; i < 9; i++){
+        var smallest = potentials[0];
+        var smallestIndex = 0;
+        var number = 0;
+
+        for(let j = 1; j < 9; j++){
+            if(potentials[j].size < smallest.size){
+                smallest = potentials[j];
+                smallestIndex = j;
+            }
+        }
+        
+        var check = true;
+        var k = 1;
+
+        while(check && k < 10){
+            if(smallest.has(k)){
+                number = k;
+                check = false;
+            }
+            k++
+        }
+
+        puzzle[lr + (Math.floor(smallestIndex/3))][lc + (smallestIndex%3)] = number;
+
+        smallest.add(10);
+        smallest.add(11);
+        smallest.add(12);
+        smallest.add(13);
+        smallest.add(14);
+        smallest.add(15);
+        smallest.add(16);
+
+        for(let j = 0; j < 9; j++){
+            potentials[j].delete(number);
+        }       
+    }
+
+}
 
 function clickTile(){
     let newTile = this;
