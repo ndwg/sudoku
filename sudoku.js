5ee3848120d2c59129db10b4974d2d2607225234
Nathan M Silva <nms204@scarletmail.rutgers.edu>
2022-06-14 16:09:13 -0400

    Cracked solution algorithm
    Developed an algorithm that generates a sudoku solution

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,6 @@
 var puzzle = [];
+var potentials = [];
+var solution = [];
 
 document.addEventListener("keydown", changeValue);
 
@@ -35,275 +37,72 @@ function setPuzzle(){
 }
 
 function chooseNumbers(){
-    configureGrid(0,3,0,3);
-    configureGrid(3,6,3,6);
-    configureGrid(6,9,6,9);
-    configureComplexGrid(0,3,6,9);
-    configureComplexGrid(6,9,0,3);
-    configureComplexGrid(0,3,3,6);
-    configureComplexGrid(3,6,0,3);
-    configureComplexGrid(3,6,6,9);
-    configureComplexGrid(6,9,3,6);
+    let establishedTiles = new Set();
+
+    for(let r = 0; r < puzzle.length; r++){
+        var row = []
+
+        for(let c = 0; c < puzzle[0].length; c++){
+            row.push(setPotentials());
+        }
+        potentials.push(row);
+    }
+
+    for(let a = 0; a < 81; a++){
+        var smallest = 10, smallestRow, smallestColumn;
+
+        for(let r = 0; r < potentials.length; r++){
+            for(let c = 0; c < potentials[0].length; c++){
+                if(!establishedTiles.has(puzzle[r][c].id) && (potentials[r][c].size < smallest || (potentials[r][c].size == smallest && Math.random() > .95))){
+                    smallest = potentials[r][c].size;
+                    smallestRow = r;
+                    smallestColumn = c;
+                }
+            }
+        }
+
+        
+        const values = Array.from(potentials[smallestRow][smallestColumn]);
+        let val = values[Math.floor(Math.random()*values.length)];
+        if(val != undefined){
+            puzzle[smallestRow][smallestColumn] = val;
+            updatePotentials(smallestRow,smallestColumn);
+            establishedTiles.add(puzzle[smallestRow][smallestColumn].id);
+        }
+    }
+
+    for(let r = 0; r < potentials.length; r++){
+        for(let c = 0; c < potentials[0].length; c++){
+            if(potentials[r][c].size != 0){
+                location.reload();
+            }
+        }
+    }
+
     console.log(puzzle);
+    console.log(potentials);
 }
 
-function configureGrid(lr,ur,lc,uc){
-    var usedNums = [];
+function setPotentials(){
+    var potentialsList = new Set();
 
-    for(let r = lr; r < ur; r++){
-        for(let c = lc; c < uc; c++){
-            let randomNum = Math.floor(Math.random()*9) + 1;
-            //puzzle[r][c].innerText = randomNum;
-            puzzle[r][c] = randomNum;
-            if(usedNums[randomNum] == 1) c--;
-            usedNums[randomNum] = 1;
-        }
+    for(let v = 1; v < 10; v++){
+        potentialsList.add(v);
     }
+    return potentialsList;
 }
 
-/*function configureComplexGrid(lr,ur,lc,uc){
-    var potentials = [];
-
-    for(let r = lr; r < ur; r++){
-        for(let c = lc; c < uc; c++){
-            var tilePotentials = new Set;
-
-            tilePotentials.add(1);
-            tilePotentials.add(2);
-            tilePotentials.add(3);
-            tilePotentials.add(4);
-            tilePotentials.add(5);
-            tilePotentials.add(6);
-            tilePotentials.add(7);
-            tilePotentials.add(8);
-            tilePotentials.add(9);
-
-            for(let i = 0; i < 9; i++){
-                tilePotentials.delete(puzzle[r][i]);
-                tilePotentials.delete(puzzle[i][c]);
-            }
-
-            potentials.push(tilePotentials);
+function updatePotentials(row, column){
+    for(let s = row - (row % 3); s < 3 + row - (row % 3); s++){
+        for(let t = column - (column % 3); t < 3 + column - (column % 3); t++){
+            potentials[s][t].delete(puzzle[row][column]);
         }
     }
 
-    console.log(potentials);
-
-    for(let i = 0; i < 9; i++){
-        var smallest = potentials[0];
-        var smallestIndex = 0;
-        var number = 0;
-
-        for(let j = 1; j < 9; j++){
-            if(potentials[j].size < smallest.size){
-                smallest = potentials[j];
-                smallestIndex = j;
-            }
-        }
-        
-        var check = true;
-        var k = 1;
-
-        while(check && k < 10){
-            if(smallest.has(k)){
-                number = k;
-                check = false;
-            }
-            k++
-        }
-
-        puzzle[lr + (Math.floor(smallestIndex/3))][lc + (smallestIndex%3)] = number;
-
-        smallest.add(10);
-        smallest.add(11);
-        smallest.add(12);
-        smallest.add(13);
-        smallest.add(14);
-        smallest.add(15);
-        smallest.add(16);
-
-        for(let j = 0; j < 9; j++){
-            potentials[j].delete(number);
-        }       
+    for(let s = 0; s < 9; s++){
+        potentials[row][s].delete(puzzle[row][column]);
+        potentials[s][column].delete(puzzle[row][column]);
     }
-}*/
-
-function configureComplexGrid(lr,ur,lc,uc){
-    var potentials = [];
-
-    for(let r = lr; r < ur; r++){
-        for(let c = lc; c < uc; c++){
-            var tilePotentials = new Set;
-
-            tilePotentials.add(1);
-            tilePotentials.add(2);
-            tilePotentials.add(3);
-            tilePotentials.add(4);
-            tilePotentials.add(5);
-            tilePotentials.add(6);
-            tilePotentials.add(7);
-            tilePotentials.add(8);
-            tilePotentials.add(9);
-
-            for(let i = 0; i < 9; i++){
-                tilePotentials.delete(puzzle[r][i]);
-                tilePotentials.delete(puzzle[i][c]);
-            }
-
-            potentials.push(tilePotentials);
-        }
-    }
-
-    console.log(potentials);
-
-    var mode = [];
-
-    for(let j = 1; j < 10; j++){
-        mode[j] = 0;
-    }
-
-    for(let j = 0; j < 9; j++){
-        if(potentials[j].has(1)) mode[1]++;
-        if(potentials[j].has(2)) mode[2]++;
-        if(potentials[j].has(3)) mode[3]++;
-        if(potentials[j].has(4)) mode[4]++;
-        if(potentials[j].has(5)) mode[5]++;
-        if(potentials[j].has(6)) mode[6]++;
-        if(potentials[j].has(7)) mode[7]++;
-        if(potentials[j].has(8)) mode[8]++;
-        if(potentials[j].has(9)) mode[9]++;
-    }
-
-    console.log(mode);
-
-    for(let i = 0; i < 9; i++){
-        var smallestMode = 10;
-        var number = 0;
-        var index = 0;
-        
-        for(let j = 1; j < 10; j++){
-            if(mode[j] < smallestMode){
-                smallestMode = mode[j];
-                number = j;
-            }
-            else if(mode[j] == smallestMode){
-                if(Math.random() > 0.75){
-                    smallestMode = mode[j];
-                    number = j;
-                }
-            }
-        }
-
-        var check = true;
-        var k = 0;
-
-        while(check && k < 9){
-            if(potentials[k].has(number)){
-                index = k;
-                check = false;
-            }
-            k++
-        }
-
-        puzzle[lr + (Math.floor(index/3))][lc + (index%3)] = number;
-
-
-        for(let j = 0; j < 9; j++){
-            potentials[j].delete(number);
-        }
-
-        for(let j = 1; j < 10; j++){
-            if(potentials[index].has(j)){
-                potentials[index].delete(j);
-                mode[j]--;
-            }
-        }
-
-        mode[number] = 11;
-    }
-
-    //check if all tiles are filled, if not get missing number
-    var missingNumbers = [];
-    var emptyTiles = [];
-    var emptyTilesIndex = 0;
-
-    for(let j = 1; j < 10; j++){
-        missingNumbers[j] = 0;
-    }
-
-    for(let r = lr; r < ur; r++){
-        for(let c = lc; c < uc; c++){
-            if(puzzle[r][c] == 1 ||puzzle[r][c] == 2||puzzle[r][c] == 3||puzzle[r][c] == 4||puzzle[r][c] == 5||puzzle[r][c] == 6||puzzle[r][c] == 7||puzzle[r][c] == 8||puzzle[r][c] == 9) missingNumbers[puzzle[r][c]] = 1;
-            else{
-                emptyTiles[emptyTilesIndex] = puzzle[r][c].id;
-                emptyTilesIndex++;
-            }
-        }
-    }
-    
-    if(Array.isArray(emptyTiles) && emptyTiles.length > 0){
-        console.log(emptyTiles);
-
-        for(let j = 0; j < emptyTiles.length; j++){
-            var coord = emptyTiles[j].split("-");
-            var row = coord[0];
-            var column = coord[1];
-
-            //check empty tiles for potential candidates
-            var tilePotentials = new Set;
-
-            tilePotentials.add(1);
-            tilePotentials.add(2);
-            tilePotentials.add(3);
-            tilePotentials.add(4);
-            tilePotentials.add(5);
-            tilePotentials.add(6);
-            tilePotentials.add(7);
-            tilePotentials.add(8);
-            tilePotentials.add(9);
-
-            for(let i = 0; i < 9; i++){
-                tilePotentials.delete(puzzle[row][i]);
-                tilePotentials.delete(puzzle[i][column]);
-            }        
-            
-            //check which candidates can swap with missing number
-            for(let r = lr; r < ur; r++){
-                for(let c = lc; c < uc; c++){
-                    var otherPotentials = new Set;
-
-                    otherPotentials.add(1);
-                    otherPotentials.add(2);
-                    otherPotentials.add(3);
-                    otherPotentials.add(4);
-                    otherPotentials.add(5);
-                    otherPotentials.add(6);
-                    otherPotentials.add(7);
-                    otherPotentials.add(8);
-                    otherPotentials.add(9);
-
-                    for(let i = 0; i < 9; i++){
-                        otherPotentials.delete(puzzle[r][i]);
-                        otherPotentials.delete(puzzle[i][c]);
-                    }
-
-                    //swap them
-                    for(let i = 1; i < 10; i++){
-                        if(missingNumbers[i] == 0 && tilePotentials.has(puzzle[r][c]) && otherPotentials.has(i)){
-                            puzzle[row][column] = puzzle[r][c];
-                            puzzle[r][c] = i;
-                            missingNumbers[i] == 1;
-                            r = 10;
-                            c = 10;
-                            i = 11;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-
 }
 
 function clickTile(){
